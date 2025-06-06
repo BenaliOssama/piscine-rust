cc76b460942e73b422a24fac260642d6b55977a4
obenali <oussama.benali@ump.ac.ma>
2025-06-06 20:09:09 +0100

    use let for normal values, use const for fixed values that never change

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,3 +1,25 @@
+use std::io ;
+
 fn main(){
-    println!("hello world")
+ 
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+
+    let mut trials : u32 = 0 ; 
+
+    let answer  = "The letter e\n";
+
+    loop {
+        print!("{}", riddle);
+        let mut guess  = String::new();
+
+        io::stdin().read_line(&mut guess).expect("error reading the line!");
+
+        trials += 1 ; 
+
+
+        if guess == answer {
+            println!("Number of trials: {}", trials );
+                return ; 
+        }
+    }
 }

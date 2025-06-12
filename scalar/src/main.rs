7d743b194766f1bfbdf88afc3aca86d9dee5ea78
obenali <oussama.benali@ump.ac.ma>
2025-06-12 14:17:50 +0100

    32.0 is the same as 32. and 1e15 is the same as 1*10^15

--- scalar/src/main.rs
+++ scalar/src/main.rs
@@ -21,3 +21,36 @@ fn main() {
     println!("rem: {}", rem(22.0, 2.0)); // 'rem: 0'
     println!("rem: {}", rem(-128.23, 2.0)); // 'rem: -0.22999573'
 }
+
+
+#[cfg(test)]
+mod tests {
+    use scalar::*;
+
+    #[test]
+    #[should_panic]
+    fn test_panic_sum() {
+        sum(25, 255);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_panic_diff() {
+        diff(-32768, 32766);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_panic_pro() {
+        pro(-128, 2);
+    }
+
+    #[test]
+    fn pass() {
+        assert_eq!(sum(1, 2), 3);
+        assert_eq!(diff(1, 2), -1);
+        assert_eq!(pro(1, 2), 2);
+        assert_eq!(quo(1.0, 2.0), 0.5);
+        assert_eq!(rem(1.0, 2.0), 1.0);
+    }
+}

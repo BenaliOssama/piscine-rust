f336ed9c31a791d72cef1c326114975b3fe8ab56
obenali <oussama.benali@ump.ac.ma>
2025-07-24 22:07:12 +0100

    feeling a little dump after checking the math of the nth fibonachi

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -9,5 +9,16 @@ fn helper(n : u32, current: u32, prev: u32) -> u32{
     let temp = current ;
     let next = current + prev ;
     helper(n - 1, next , temp)
-
 }
+
+
+// the matimatical definition
+// fib(0) = 0
+// fib(1) = 1
+// fib(n) = fib(n - 1) + fib(n - 2)
+//
+// so matches!(n, 0 | 1) checks if n is 0 or 1
+//
+// otherwise fibonacci(n-2) + fibonacci(n-1)
+// beauty in math and computing.
+

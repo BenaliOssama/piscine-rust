fec42d27e94338511c41829297b5ead15c47da6b
obenali <oussama.benali@ump.ac.ma>
2025-07-24 15:20:39 +0100

    done done bon

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -3,15 +3,15 @@ use std::io;
 fn main() {
     let target = String::from("The letter e");
     let mut number : u16 = 0 ;
-    loop {
+    let tried = loop {
         number += 1 ;
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         let mut input = String::new();
         io::stdin().read_line(&mut input);
 
         if target == input.trim() {
-            println!("Number of trials: {}", number);
-            break;
+            break number ;
         }
-    }
+    };
+    println!("Number of trials: {}", tried);
 }

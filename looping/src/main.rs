fe1457698c008091da9b9cc723733d8167687f60
obenali <oussama.benali@ump.ac.ma>
2025-07-24 15:16:59 +0100

    add numbers

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -2,12 +2,15 @@ use std::io;
 
 fn main() {
     let target = String::from("The letter e");
+    let mut number u16;
     loop {
+        number += 1 ;
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         let mut input = String::new();
         io::stdin().read_line(&mut input);
 
         if target == input.trim() {
+            println!("Number of trials: {number}", number);
             break;
         }
     }

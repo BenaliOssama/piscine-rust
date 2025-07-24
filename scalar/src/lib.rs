34fd974192b3c3ffd43dce37e6d6ff00357bfac1
obenali <oussama.benali@ump.ac.ma>
2025-07-24 12:20:50 +0100

    scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum(a: u8, b:u8 ) -> u8{
+    a + b 
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b 
+}
+
+pub fn pro(a: i32, b: i32) -> i32 {
+    a*b
+}
+
+pub fn quo(a: f32, b: f32) -> i32 {
+     (a / b) as i32
+}
+
+pub fn rem(a: f32, b: f32) -> u32 {
+    (a % b) as u32
+}

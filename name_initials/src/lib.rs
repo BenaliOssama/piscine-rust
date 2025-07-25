3d5c8b6e4710967e34baa82994737ae39f7635e9
obenali <oussama.benali@ump.ac.ma>
2025-07-25 18:37:40 +0100

    you know where it is

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -7,7 +7,7 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
         let chars : Vec<char> = name.split_ascii_whitespace()
             .map(|x| x.chars().next().unwrap())
             .collect();
-        result.push(format!{"{}. {}", chars[0], chars[1]});
+        result.push(format!{"{}. {}.", chars[0], chars[1]});
     }
     result
 }

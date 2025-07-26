5af9961db493e2c6d37d2facb5c6050ed24b843f
obenali <oussama.benali@ump.ac.ma>
2025-07-26 16:06:14 +0100

    don't be the reason

---
+++ arrange_it/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn arrange_phrase(phrase: &str) -> String {
+        let mut stack : Vec<String> = vec![];
+        let mut fragment : Vec<&str> = phrase.split_whitespace().collect(); 
+
+        while fragment.len() > 0 {
+            for (i, e) in fragment.iter().enumerate() {
+
+                let index = stack.len() as usize  + 1;
+                let index_str = &index.to_string();
+
+                if e.contains(index_str) {
+                    stack.push(e.replace(index_str, "").to_string());
+                    fragment.remove(i);
+                    break;
+                }
+            }
+        } 
+    return stack.join(" ");
+}

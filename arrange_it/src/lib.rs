7771328dc1cc14a0c50186c627448e66ed376d9e
obenali <oussama.benali@ump.ac.ma>
2025-07-26 17:27:28 +0100

    try with remove

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn arrange_phrase(phrase: &str) -> String {
 
     let mut stack : Vec<String> = vec![];
-    let fragment : Vec<&str> = phrase.split_whitespace().collect(); 
+    let mut fragment : Vec<&str> = phrase.split_whitespace().collect(); 
 
-    while fragment.len() != stack.len() {
-        for (_, e) in fragment.iter().enumerate() {
+    while fragment.len() > 0 {
+        for (i, e) in fragment.iter().enumerate() {
 
             let index = stack.len() as usize  + 1;
             let index_str = &index.to_string();
 
             if e.contains(index_str){
                 stack.push(e.replace(index_str, ""));
-                //fragment.remove(i);
+                fragment.remove(i);
                 break;
             }
         }

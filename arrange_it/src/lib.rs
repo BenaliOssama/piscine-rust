adf3696829965dcf9c034635ecb55741ef6d23f1
obenali <oussama.benali@ump.ac.ma>
2025-07-26 17:07:41 +0100

    try without removing

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,19 +1,19 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-        let mut stack : Vec<String> = vec![];
-        let mut fragment : Vec<&str> = phrase.split_whitespace().collect(); 
+    let mut stack : Vec<String> = vec![];
+    let mut fragment : Vec<&str> = phrase.split_whitespace().collect(); 
 
-        while fragment.len() > 0 {
-            for (i, e) in fragment.iter().enumerate() {
+    while fragment.len() != stack.len() {
+        for (i, e) in fragment.iter().enumerate() {
 
-                let index = stack.len() as usize  + 1;
-                let index_str = &index.to_string();
+            let index = stack.len() as usize  + 1;
+            let index_str = &index.to_string();
 
-                if e.contains(index_str) {
-                    stack.push(e.replace(index_str, "").to_string());
-                    fragment.remove(i);
-                    break;
-                }
+            if e.contains(index_str){
+                stack.push(e.replace(index_str, "").to_string());
+                //fragment.remove(i);
+                break;
             }
-        } 
+        }
+    } 
     return stack.join(" ");
 }

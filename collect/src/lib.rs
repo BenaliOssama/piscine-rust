266db5721ec0dd2a3b7560c2197644244778abda
obenali <oussama.benali@ump.ac.ma>
2025-07-27 22:59:07 +0100

    don't panic stay strong

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn bubble_sort(arr: &mut [i32]) {
     for j in 0..arr.len() {
         for i in j..arr.len() {
-            if arr[i] > arr[j] {
+            if arr[i] < arr[j] {
                 arr.swap(i,j);
             }
         }

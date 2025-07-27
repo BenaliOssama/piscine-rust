5bc2b8bb9bf077ba3a365861f8a5ed04683243d7
obenali <oussama.benali@ump.ac.ma>
2025-07-27 21:24:06 +0100

    tired more code instead of less code but less thinking :)

---
+++ string_permutation/src/lib.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    let mut freq : HashMap<char, isize> = HashMap::new();
+
+    for char in s1.chars() {
+        match freq.get(&char) {
+            Some(num) => {
+                freq.insert(char, num + 1 );
+            },
+             _ => {freq.insert(char, 1) ;} ,
+        }
+    }
+
+    
+    for char in s2.chars(){
+        match freq.get(&char) {
+            Some(num) => {
+                freq.insert(char, num - 1);
+            },
+            None => {
+                ()
+            }
+        }
+    }
+
+    for (word , count ) in freq {
+        if count != 0 {
+            return false;
+        }
+    }
+    true
+}

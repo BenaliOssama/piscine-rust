6e8d72f962574d2674a53a11327d597f04e90bbe
obenali <oussama.benali@ump.ac.ma>
2025-07-27 15:35:47 +0100

    what are you doing

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,34 +1,79 @@
+use rand::prelude::*;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
 }
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum Rank {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Number(u8),
 }
 
 impl Suit {
     pub fn random() -> Suit {
-    todo!()
+        let mut rng = rand::thread_rng();
+        let mut Deck : Vec<Suit> = vec![Suit::Heart, Suit::Diamond, Suit::Spade, Suit::Club];
+        Deck.shuffle(&mut rng);
+        Deck[0]
     }
 
     pub fn translate(value: u8) -> Suit {
-    todo!()
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond, 
+            3 => Suit::Spade, 
+            4 => Suit::Club, 
+            _ => panic!("over the limit")
+        }
     }
 }
 
 impl Rank {
     pub fn random() -> Rank {
-    todo!()
+        let mut rng = rand::thread_rng();
+        let mut rank : Vec<Rank> = vec![Rank::Ace, Rank::King, Rank::Queen, Rank::Jack];
+        for i in 2..=10 {
+            rank.push(Rank::Number(i as u8));
+        }
+        rank.shuffle(&mut rng);
+        rank[0]
     }
 
     pub fn translate(value: u8) -> Rank {
-    todo!()
+        match value {
+                2..=10 => Rank::Number(value),
+                1 =>  Rank::Ace , 
+                11 => Rank::Jack, 
+                12 => Rank::Queen, 
+                13 => Rank::King,
+                _ => panic!("rank over the limit")
+        }
     }
 }
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
 pub fn winner_card(card: &Card) -> bool {
-    todo!()
+    //card == &Card{suit:Suit::Spade, rank:Rank::Ace}
+    match card.suit {
+        Suit::Spade =>  
+        {
+            match card.rank {
+                Rank::Ace => true,
+                _ => false ,
+            }
+        },
+        _ => false,
+    }
 }

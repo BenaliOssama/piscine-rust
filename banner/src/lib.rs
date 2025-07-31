5bfd0a44e0a4ea9c614140ded9ad0c41d71cc0c5
obenali <oussama.benali@ump.ac.ma>
2025-07-31 23:17:28 +0100

    done there

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -2,11 +2,18 @@ use std::{collections::HashMap, num::ParseFloatError};
 
 pub struct Flag {
     // expected public fields
+    short_hand: String,
+    long_hand: String,
+    desc: String,
 }
 
-impl<'a> Flag<'a> {
-    pub fn opt_flag(name: &'a str, d: &'a str) -> Self {
-        todo!()
+impl Flag {
+    pub fn opt_flag(name: & str, d: & str) -> Self {
+        return Flag{
+            short_hand: name.chars().next().unwrap().to_string(),
+            long_hand: name.to_string(),
+            desc: d.to_string(),
+        }
     }
 }
 
@@ -18,18 +25,31 @@ pub struct FlagsHandler {
 
 impl FlagsHandler {
     pub fn add_flag(&mut self, flag: Flag, func: Callback) {
-        todo!()
+        self.flags.insert( flag.long_hand, func);
+        self.flags.insert(flag.short_hand, func);
     }
 
     pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
-        todo!()
+        let mut arg : String =  String::from(input);
+        let arg = arg.replace("-", "");
+        let callback = self.flags.get(&arg).unwrap();
+        match callback(argv[0], argv[1]) {
+            Ok(res) => Ok(res),
+            Err(res) => Err(String::from("invalid float literal")),
+        }
     }
 }
 
 pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    todo!()
+    let na = a.parse::<f64>()?;
+    let nb = b.parse::<f64>()?;
+    let res = (na/nb).to_string(); 
+    Ok(res)
 }
 
 pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    todo!()
+    let na = a.parse::<f64>()?;
+    let nb = b.parse::<f64>()?;
+    let res = (na % nb).to_string(); 
+    Ok(res)
 }

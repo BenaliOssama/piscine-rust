71bdc7bfcf25eb5c20d3e0bb74b4557b27ecb528
obenali <oussama.benali@ump.ac.ma>
2025-07-31 13:40:11 +0100

    br

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,22 @@
+
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => server.unwrap().to_string(),
+        Security::Message => server.expect("ERROR: program stops").to_string(),
+        Security::Warning => server.unwrap().to_string(),
+        Security::NotFound => server.unwrap_or("WARNING: check the server").to_string(),
+        Security::UnexpectedUrl => match server {
+            Err(err) => err.to_string(),
+
+            Ok(err) => panic!("{err:?}"),
+        }
+    }
+}

1aabec9de14c19e2a47eb38216168bb2ec688492
obenali <oussama.benali@ump.ac.ma>
2025-07-31 18:17:17 +0100

    OpenOptions

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,9 +1,14 @@
+use std::fs::OpenOptions;
 use std::path::Path;
-use std::fs::File;
+use std::io::Write;
+
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let mut file = File::open(path).unwrap_or_else(|_|{
-        File::create(path).unwrap();
-        File::open(path).unwrap()
-    });
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path).unwrap();
+
+    file.write_all(content.as_bytes());
 }

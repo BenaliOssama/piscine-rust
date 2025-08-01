efae27e04d51b0ae62fdaa75ebe133f1ca816fe5
obenali <oussama.benali@ump.ac.ma>
2025-08-01 12:01:32 +0100

    ciphered

---
+++ cipher/src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    expected: String,
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    // get the original from the cipher
+    let mut cc  = "".to_string();
+    for char in original.chars() {
+       if char as u8 <= 'Z' as u8 && char >= 'A' {
+            cc.push(( 'A' as u8 + ('Z' as u8 - char as u8)) as char);
+       } else if char as u8 <= 'z' as u8 && char >= 'a' {
+            cc.push(( 'a' as u8 + ('z' as u8 - char as u8)) as char);
+       }else{
+           cc.push(char);
+       }
+    }
+    if &cc == ciphered {
+        return Ok(());
+    }else{
+        return Err(CipherError{ expected: format!("expected: {}", cc)});
+    }
+}

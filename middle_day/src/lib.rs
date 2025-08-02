ffcf518355d23c3c1d9ec6b70a0c3e997f3ce718
obenali <oussama.benali@ump.ac.ma>
2025-08-02 14:41:12 +0100

    middle day

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,13 @@
+use chrono::{NaiveDate, Datelike};
+
+pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
+
+    let is_leap = NaiveDate::from_ymd_opt(year as i32, 2, 29).is_some();
+
+    let middle_day = if is_leap{ 184 } else { 183 };
+
+    //-------------------> year + ordinal day
+    let middle = NaiveDate::from_yo_opt(year as i32, middle_day).unwrap();
+    
+    Some(middle.weekday())
+}

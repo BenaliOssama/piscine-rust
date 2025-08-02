4f1a8d5b12576b8f7f6e57bfb00b27ae627c5f3b
obenali <oussama.benali@ump.ac.ma>
2025-08-02 14:58:21 +0100

    suppose there is no middle day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -4,7 +4,11 @@ pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
 
     let is_leap = NaiveDate::from_ymd_opt(year as i32, 2, 29).is_some();
 
-    let middle_day = if is_leap{ 184 } else { 183 };
+    let middle_day = if is_leap{ 
+        return None
+    } else { 
+        183
+    };
 
     //-------------------> year + ordinal day
     let middle = NaiveDate::from_yo_opt(year as i32, middle_day).unwrap();

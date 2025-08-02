d661499f750c9b493bc360a0f210f2b97c749ca4
obenali <oussama.benali@ump.ac.ma>
2025-08-02 15:10:29 +0100

    does it fit start

---
+++ does_it_fit/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn area_fit(
+    (x, y): (usize, usize),
+    kind: areas_volumes::GeometricalShapes,
+    times: usize,
+    (a, b): (usize, usize),
+) -> bool {
+    todo!()
+}
+
+pub fn volume_fit(
+    (x, y, z): (usize, usize, usize),
+    kind: areas_volumes::GeometricalVolumes,
+    times: usize,
+    (a, b, c): (usize, usize, usize),
+) -> bool {
+    todo!()
+}

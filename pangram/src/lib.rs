1e3f5b9f28a7d0a6d01987830a6d6352f80c6123
obenali <oussama.benali@ump.ac.ma>
2025-08-03 17:40:17 +0100

    mybe not

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn is_pangram(s: &str) -> bool {
-    for c in ('a' as u8 .. 'z' as u8) {
+    for c in ('a' as u8 ..= 'z' as u8) {
         if ! s.to_lowercase().contains(c as char){
             return false;
         }

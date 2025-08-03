69be6932840213eaecc20f66efd0595fc744fb88
obenali <oussama.benali@ump.ac.ma>
2025-08-03 19:01:58 +0100

    alpha barik

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -6,13 +6,13 @@ pub fn talking(text: &str) -> &str {
     
     }
     if text.trim().chars().last().unwrap() == '?' {
-        if text.chars().all(|c| c.is_uppercase() || !c.is_ascii() ) {
+        if text.chars().all(|c| c.is_uppercase() || !c.is_alphabetic() ) {
             return "Quiet, I am thinking!";
         }else{
             return "Sure.";
         }
     }
-    if text.trim().chars().all(|c| c.is_uppercase() || !c.is_ascii()) {
+    if text.trim().chars().all(|c| c.is_uppercase() || !c.is_alphabetic()) {
         return "LEAVE ME ALONE!";
     }
 

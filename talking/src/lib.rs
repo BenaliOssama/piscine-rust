93e6ca24c6242c7796a0b9a71674f37e8856120f
obenali <oussama.benali@ump.ac.ma>
2025-08-03 18:53:14 +0100

    be lazy

---
+++ talking/src/lib.rs
@@ -0,0 +1,20 @@
+
+
+pub fn talking(text: &str) -> &str {
+    if text.len()  == 0 {
+        return "Just say something!";
+    
+    }
+    if text.chars().last().unwrap() == '?' {
+        if text.chars().all(|c| c.is_uppercase() || !c.is_ascii() ) {
+            return "Quiet, I am thinking!";
+        }else{
+            return "Sure.";
+        }
+    }
+    if text.chars().all(|c| c.is_uppercase() || c.is_ascii()) {
+        return "LEAVE ME ALONE!";
+    }
+
+    return "Interesting";
+}

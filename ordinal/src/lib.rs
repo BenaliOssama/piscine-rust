90c8d23f5b22187c56ed50849e28a641d37c7cc8
obenali <oussama.benali@ump.ac.ma>
2025-08-03 17:20:32 +0100

    try again

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,12 +1,16 @@
 pub fn num_to_ordinal(x: u32) -> String {
         match x {
-           x if  x.to_string().ends_with("11") => x.to_string() + "st",
-           x if  x.to_string().ends_with("12") => x.to_string() + "nd",
-           x if  x.to_string().ends_with("13") => x.to_string() + "rd",
+           x if  x.to_string().ends_with("11") && x.to_string().len() > 2 => x.to_string() + "st",
+           x if  x.to_string().ends_with("12") && x.to_string().len() > 2=> x.to_string() + "nd",
+           x if  x.to_string().ends_with("13") && x.to_string().len() > 2=> x.to_string() + "rd",
 
-           x if  x.to_string().ends_with("1") => x.to_string() + "st",
-           x if  x.to_string().ends_with("2") => x.to_string() + "nd",
-           x if  x.to_string().ends_with("3") => x.to_string() + "rd",
+           x if  x == 11  => x.to_string() + "th",
+           x if  x == 12  => x.to_string() + "th",
+           x if  x == 13  => x.to_string() + "th",
+
+           x if  x == 1 => x.to_string() + "st",
+           x if  x == 2 => x.to_string() + "nd",
+           x if  x == 3 => x.to_string() + "rd",
            _ => x.to_string() + "th",
     }
 }

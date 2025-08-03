910f722d8b31e150d831ec3273090e1d6e3c6b4f
obenali <oussama.benali@ump.ac.ma>
2025-08-03 16:07:12 +0100

    push

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
     for (i , num) in array.iter().rev().enumerate() {
         if key == *num {
-            return Some(i); 
+            return Some(array.len() - i); 
         }
     }
     None

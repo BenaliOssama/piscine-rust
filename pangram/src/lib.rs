5a92ce0925c3fc8b323e0973b652a55ec0f99200
obenali <oussama.benali@ump.ac.ma>
2025-08-03 17:39:10 +0100

    we are the  beeeeast

---
+++ pangram/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn is_pangram(s: &str) -> bool {
+    for c in ('a' as u8 .. 'z' as u8) {
+        if ! s.to_lowercase().contains(c as char){
+            return false;
+        }
+    }
+    true
+}

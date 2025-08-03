bc438f90711ef6934d3360413dc16971a88449ab
obenali <oussama.benali@ump.ac.ma>
2025-08-03 17:00:59 +0100

    more fixing

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,5 +1,9 @@
 pub fn num_to_ordinal(x: u32) -> String {
         match x {
+           x if  x.to_string().ends_with("11") => x.to_string() + "st",
+           x if  x.to_string().ends_with("12") => x.to_string() + "nd",
+           x if  x.to_string().ends_with("13") => x.to_string() + "rd",
+
            x if  x.to_string().ends_with("1") => x.to_string() + "st",
            x if  x.to_string().ends_with("2") => x.to_string() + "nd",
            x if  x.to_string().ends_with("3") => x.to_string() + "rd",

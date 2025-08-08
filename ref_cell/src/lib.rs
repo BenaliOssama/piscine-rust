ee7ab3cde7ddecbaf6b55deb793267eebefc09cc
obenali <oussama.benali@ump.ac.ma>
2025-08-08 23:41:48 +0100

    try this fix

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -13,20 +13,18 @@ impl Tracker {
         Tracker{messages: RefCell::new(vec![]), value : RefCell::new(0) , max: max }
     }
 
-    pub fn set_value(&self, value : &Rc<usize>) {
+    pub fn set_value(&self, value: &Rc<usize>) {
         let new_value = Rc::strong_count(value);
 
-        if new_value * 100 / self.max >=  75 && new_value < self.max {
-
+        if new_value * 100 / self.max >= 75 && new_value <= self.max {
             self.messages.borrow_mut().push(
-                format!("Warning: You have used up over {}% of your quota!",
-                new_value * 100 / (self.max ) )
+                format!("Warning: You have used up over {}% of your quota!", new_value * 100 / self.max)
             );
-
-        }else if new_value > self.max {
+        } else if new_value > self.max {
             self.messages.borrow_mut().push("Error: You can't go over your quota!".to_string());
         }
-        *self.value.borrow_mut() = new_value; 
+
+        *self.value.borrow_mut() = new_value;
     }
 
     pub fn peek(&self, arg: &Rc<usize>){

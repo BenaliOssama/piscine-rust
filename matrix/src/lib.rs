584edd5f620e1af319acc14de626344a47ce6f53
obenali <oussama.benali@ump.ac.ma>
2025-08-15 16:24:35 +0100

    try first

---
+++ matrix/src/lib.rs
@@ -0,0 +1,45 @@
+use lalgebra_scalar::Scalar;
+
+#[derive(Debug)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl <T: Scalar<Item = T>> Matrix<T> {
+	pub fn new() -> Matrix<T> {
+            Matrix(vec![vec![<T as Scalar>::zero()]])
+	}
+
+	pub fn identity(n: usize) -> Matrix<T> {
+            let mut m = Matrix(vec![vec![<T as Scalar>::zero() ;n]; n]);
+            for i in 0..n {
+                m.0[i][i] = <T as Scalar>::one();
+            }
+            m
+	}
+
+	pub fn zero(row: usize, col: usize) -> Matrix<T> {
+            Matrix(vec![vec![<T as Scalar>::zero() ;col]; row])
+	}
+
+}
+
+
+//  `Add`, `Sub`, `Mul`, `Div` 
+//
+//
+// trait heloo
+// trait world 
+//
+// type humn 
+//
+// imple hello for hum 
+// gn say_hello -> hello man
+//
+// impl world for hum 
+// fn say_hell self -> hello bro
+//
+// let ghost = humn 
+//
+// ghost.say_hello
+//
+//
+// <ghost as Hello>::say_hello

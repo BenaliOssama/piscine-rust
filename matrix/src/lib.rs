406f071fb87b7daf9ffab5e40b9f6454fed6e94f
obenali <oussama.benali@ump.ac.ma>
2025-08-15 20:19:22 +0100

    add fake triats to matrix

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,43 +1,70 @@
 use lalgebra_scalar::Scalar;
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Debug, PartialEq)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-impl <T: Scalar<Item = T>> Matrix<T> {
-	pub fn new() -> Matrix<T> {
-            Matrix(vec![vec![<T as Scalar>::zero()]])
-	}
+impl<T: Scalar<Item = T>> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![<T as Scalar>::zero()]])
+    }
 
-	pub fn identity(n: usize) -> Matrix<T> {
-            let mut m = Matrix(vec![vec![<T as Scalar>::zero() ;n]; n]);
-            for i in 0..n {
-                m.0[i][i] = <T as Scalar>::one();
-            }
-            m
-	}
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut m = Matrix(vec![vec![<T as Scalar>::zero(); n]; n]);
+        for i in 0..n {
+            m.0[i][i] = <T as Scalar>::one();
+        }
+        m
+    }
 
-	pub fn zero(row: usize, col: usize) -> Matrix<T> {
-            Matrix(vec![vec![<T as Scalar>::zero() ;col]; row])
-	}
-
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![<T as Scalar>::zero(); col]; row])
+    }
 }
 
+impl<T: Scalar> Add for Matrix<T> {
+    type Output = Matrix<T>;
+    fn add(self, _: Matrix<T>) -> <Self as Add<Matrix<T>>>::Output {
+        panic!("no implementation for Matrix + Matrix");
+    }
+}
 
-//  `Add`, `Sub`, `Mul`, `Div` 
+impl<T: Scalar> Sub for Matrix<T> {
+    type Output = Matrix<T>;
+    fn sub(self, _: Matrix<T>) -> <Self as Add<Matrix<T>>>::Output {
+        panic!("no implementation for Matrix - Matrix");
+    }
+}
+
+impl<T: Scalar> Div for Matrix<T> {
+    type Output = Matrix<T>;
+    fn div(self, _: Matrix<T>) -> <Self as Add<Matrix<T>>>::Output {
+        panic!("no implementation for Matrix / Matrix");
+    }
+}
+
+impl<T: Scalar> Mul for Matrix<T> {
+    type Output = Matrix<T>;
+    fn mul(self, _: Matrix<T>) -> <Self as Add<Matrix<T>>>::Output {
+        panic!("no implementation for Matrix * Matrix");
+    }
+}
+
+//  `Add`, `Sub`, `Mul`, `Div`
 //
 //
 // trait heloo
-// trait world 
+// trait world
 //
-// type humn 
+// type humn
 //
-// imple hello for hum 
+// imple hello for hum
 // gn say_hello -> hello man
 //
-// impl world for hum 
+// impl world for hum
 // fn say_hell self -> hello bro
 //
-// let ghost = humn 
+// let ghost = humn
 //
 // ghost.say_hello
 //

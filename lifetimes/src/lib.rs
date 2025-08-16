2bb8847425f1c4f4630176f5fd9302b8f1e9f065
obenali <oussama.benali@ump.ac.ma>
2025-08-16 10:36:16 +0100

    lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+impl<'a> Person<'a> {
+	pub fn new<'b>(name:  &str) -> Person<'_> {
+        return Person{name, age: 0};
+	}
+}

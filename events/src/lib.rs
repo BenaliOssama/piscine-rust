27ee7734ba416fd12a0d3f3a1535d278c6d4fd84
obenali <oussama.benali@ump.ac.ma>
2025-08-16 11:49:46 +0100

    test

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -1,6 +1,5 @@
 use chrono::Duration;
 use colored::*;
-use std::fmt::Formatter;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Position {
@@ -28,29 +27,52 @@ pub enum Event<'a> {
 use std::fmt;
 
 impl fmt::Display for Notification {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-         let (r, g, b) = self.color;
-        write!(f, "{}", self.content.truecolor(r,g,b))?;
-         Ok(())
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (r, g, b) = self.color;
+        let colored_content = self.content.truecolor(r, g, b);
+        write!(f, "({:?}, {}, {})", self.position, self.size, colored_content)
     }
 }
 
 use Event::*;
-use Event::*;
-impl<'a> Event<'a> {
-    pub fn notify(&self) -> Notification {
-        let content = match self {
-            Event::Remainder(msg) => format!("Reminder: {}", msg),
-            Event::Registration(dur) => format!("Registration in {} seconds", dur.num_seconds()),
-            Event::Appointment(who) => format!("Appointment with {}", who),
-            Event::Holiday => "Holiday!".to_string(),
-        };
 
-        Notification {
-            size: 50,
-            color: (50, 50, 50),
-            position: Position::Bottom,
-            content,
+impl Event<'_> {
+    pub fn notify(&self) -> Notification {
+        match self {
+            Remainder(text) => Notification {
+                size: 50,
+                color: (50, 50, 50),
+                position: Position::Bottom,
+                content: text.to_string(),
+            },
+            Registration(duration) => {
+                let total_secs = duration.num_seconds();
+                let hours = total_secs / 3600;
+                let minutes = (total_secs % 3600) / 60;
+                let seconds = total_secs % 60;
+
+                Notification {
+                    size: 30,
+                    color: (255, 2, 22),
+                    position: Position::Top,
+                    content: format!(
+                        "You have {}H:{}M:{}S left before the registration ends",
+                        hours, minutes, seconds
+                    ),
+                }
+            }
+            Appointment(text) => Notification {
+                size: 100,
+                color: (200, 200, 3),
+                position: Position::Center,
+                content: text.to_string(),
+            },
+            Holiday => Notification {
+                size: 25,
+                color: (0, 255, 0),
+                position: Position::Top,
+                content: "Enjoy your holiday".to_string(),
+            },
         }
     }
 }

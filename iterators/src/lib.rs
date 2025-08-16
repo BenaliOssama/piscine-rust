ea439f8acf5e8137ff03a4b00a29301ff245bdce
obenali <oussama.benali@ump.ac.ma>
2025-08-16 19:10:57 +0100

    lsajdf

---
+++ iterators/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Collatz {
+    pub v: u64,
+}
+
+impl Iterator for Collatz {
+    type Item = Collatz;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v <= 1 {
+            return None;
+        }
+        let num = self.v;
+        if num | 1 != num {
+            self.v /= 2;
+        } else {
+            self.v = 3 * num + 1;
+        }
+        Some(Collatz { v: num })
+    }
+}
+
+impl Collatz {
+    pub fn new(n: u64) -> Self {
+        Self { v: n }
+    }
+}
+
+pub fn collatz(n: u64) -> usize {
+    if n < 2 {
+        return 0;
+    }
+    let mut count = 0;
+    let mut n = n;
+    while n != 1 {
+        if n | 1 != n{
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        count += 1;
+    }
+    count
+}
